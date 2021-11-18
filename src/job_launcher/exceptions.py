7bb9c738ea4a7d2c8f3699fcec6400375f090d65
Dewerton <picset@yandex.ru>
2021-11-18 18:06:32 +0300

    Improve exceptions

--- src/job_launcher/exceptions.py
+++ src/job_launcher/exceptions.py
@@ -1,9 +1,7 @@
 class JobLauncherApplicationException(Exception):
-    def __init__(self, message: str):
-        super().__init__(message)
+    pass
 
 
-class JenkinsServerException(Exception):
-    def __init__(self, message: str):
-        super().__init__(message)
+class JenkinsServerException(JobLauncherApplicationException):
+    pass
 
